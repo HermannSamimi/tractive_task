5872c2c931c65e9ad10e037b1baf81feb859e922
Hermann Samimi <hermannsamimi@gmail.com>
2025-07-10 07:17:25 +0000

    casting

--- staging/load_data.sql
+++ staging/load_data.sql
@@ -5,13 +5,13 @@
 ) }}
 
 SELECT
-  _id,
-  purchase_id,
-  event_name,
-  payment_plan_id,
-  subscription_id,
-  user_id,
-  created_at,
+  CAST(_id AS STRING) AS _id,
+  CAST(purchase_id AS STRING) AS purchase_id,
+  CAST(event_name AS STRING) AS event_name,
+  CAST(payment_plan_id AS STRING) AS payment_plan_id,
+  CAST(subscription_id AS STRING) AS subscription_id,
+  CAST(user_id AS STRING) AS user_id,
+  CAST(created_at AS TIMESTAMP) AS created_at,
   current_timestamp() AS modeling_ingestion_timestamp
 FROM {{ source('raw', 'RAW_DATA') }}
 
